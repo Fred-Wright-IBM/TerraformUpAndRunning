6700a8c07b96f9a8372fdb2e176491e3d483e679
Freddie Wright <freddie.wright@ibm.com@Freddies-MacBook-Pro.local>
2020-04-06 15:01:21 +0100

    Adding in the main.tf file updates and adding in a .gitignore file

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-006a0174c6c25ac06"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "terraform-example-FW"
+    }
 }
